5559d66300561c8859a27e0c4b853d4ba8b6d69e
Flavio Garcia <piraz@candango.org>
2025-01-29 15:38:05 -0500

    chore: add 5 to padding left, right, and top

--- wezterm.lua
+++ wezterm.lua
@@ -56,9 +56,9 @@ local config = {
         { key = "n", mods = "SHIFT|ALT", action = act.ToggleFullScreen },
     },
     window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
+        left = 5,
+        right = 5,
+        top = 5,
         bottom = 0,
     },
 }
