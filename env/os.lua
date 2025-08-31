aca74b61536889ae3aad6a709072387135cda6a5
Flavio Garcia <piraz@candango.org>
2025-08-31 01:28:58 -0400

    refactor(env): simplify functions checking os type

--- env/os.lua
+++ env/os.lua
@@ -1,27 +1,17 @@
--- Inspired by: https://bit.ly/3Y7sE3v
 local wezterm = require("wezterm")
 
 local M = {}
 
 function M.is_linux()
-    if string.find(wezterm.target_triple, "linux") then
-        return true
-    end
-    return false
+    return string.find(wezterm.target_triple, "linux")
 end
 
 function M.is_mac()
-    if string.find(wezterm.target_triple, "darwin") then
-        return true
-    end
-    return false
+    return string.find(wezterm.target_triple, "darwin")
 end
 
 function M.is_windows()
-    if string.find(wezterm.target_triple, "windows") then
-        return true
-    end
-    return false
+    return string.find(wezterm.target_triple, "windows")
 end
 
 return M
